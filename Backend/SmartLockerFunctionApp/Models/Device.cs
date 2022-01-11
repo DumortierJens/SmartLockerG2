0a614610b1c117a3c69d88e14ed5d1fb2dd61cd3
Mattice Struyve <mattice.struyve@student.howest.be>
2022-01-11 15:18:30 +0100

    New project

---
+++ Backend/SmartLockerFunctionApp/Models/Device.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartLockerFunction.Models
+{
+    public class Device
+    {
+        public string DeviceName { get; set; }
+        public string Status { get; set; }
+        public DateTime Timestamp { get { return Timestamp; } set { Timestamp = DateTime.UtcNow; } }
+    }
+
+}
