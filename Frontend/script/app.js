d2a0bd3db409115d32147872d746da537007ec65
Michiel Deseck <michiel.deseck@student.howest.be>
2022-01-13 11:21:40 +0100

    added script to html

--- Frontend/script/app.js
+++ Frontend/script/app.js
@@ -5,7 +5,7 @@ const showBestemmishowLockerngen = function(jsonObject) {
 };
 
 const init = function() {
-    handleData(`http://127.0.0.1:5000/api/v1/bestemmingen`, showLocker);
+    handleData(`https://smartlockerfunctions.azurewebsites.net/api/lockers`, showLocker);
 };
 
 document.addEventListener('DOMContentLoaded', function() {
