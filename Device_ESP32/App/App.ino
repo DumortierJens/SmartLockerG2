45e8486f57504d7de09a6f637fc582027d8b02d0
Jens Dumortier <jens.dumortier@student.howest.be>
2022-01-13 16:41:17 +0100

    Merge

--- Device_ESP32/App/App.ino
+++ Device_ESP32/App/App.ino
@@ -9,7 +9,8 @@
 #define LOCK_FEEDBACK_PIN       23
 
 // Const values
-#define API_URL                 "https://smartlockerfunctions.azurewebsites.net/api"
+#define IOTHUB_CONNSTRING       "HostName=smartlockeriothub.azure-devices.net;DeviceId=11cf21d4-03ef-4e0a-8a17-27c26ae80abd;SharedAccessKey=76vh2TvxkhMTTdQqsgM+yPIMaIXgCs03ryoCSmFRRt4="
+#define IOTHUB_DEVICEID         "11cf21d4-03ef-4e0a-8a17-27c26ae80abd"
 #define US_THRESHOLD_DIFF       5
 
 
@@ -17,11 +18,10 @@
 #include <WiFiManager.h>  // https://github.com/tzapu/WiFiManager
 #include <TaskScheduler.h>
 #include <NewPing.h>
-#include <HTTPClient.h>
-#include <WiFiClientSecure.h>
 #include <ArduinoJson.h>
 #include "Lock.h"
-#include "CA_certificates.h"
+#include "AzureIotHub.h"
+#include "Esp32MQTTClient.h"
 
 
 // Methods for tasks
@@ -41,40 +41,57 @@ NewPing usRight(US_RIGHT_TRIGGER_PIN, US_RIGHT_ECHO_PIN, 400);
 double usRightThreshold = 22;
 bool usRightLastState = false;
 
-Lock lock(LOCK_PIN, LOCK_FEEDBACK_PIN);
+LockerLock lock(LOCK_PIN, LOCK_FEEDBACK_PIN);
 bool lockLastState = LOW;
 
+
+// IoT hub
+static void SendConfirmationCallback(IOTHUB_CLIENT_CONFIRMATION_RESULT result)
+{
+  if (result == IOTHUB_CLIENT_CONFIRMATION_OK)
+  {
+    Serial.println("Send Confirmation Callback finished.");
+  }
+}
+
+static int  DeviceMethodCallback(const char *methodName, const unsigned char *payload, int size, unsigned char **response, int *response_size)
+{
+  LogInfo("Try to invoke method %s", methodName);
+  const char *responseMessage = "\"Successfully invoke device method\"";
+  int result = 200;
+
+  if (strcmp(methodName, "open") == 0)
+  {
+    LogInfo("Open lock");
+    lock.openLock();
+  }
+  else
+  {
+    LogInfo("No method %s found", methodName);
+    responseMessage = "\"No method found\"";
+    result = 404;
+  }
+
+  *response_size = strlen(responseMessage) + 1;
+  *response = (unsigned char *)strdup(responseMessage);
+
+  return result;
+}
 
 // Tasks
 Task checkMaterialTask(500, TASK_FOREVER, &checkMaterial, NULL );
 Task checkLockTask(500, TASK_FOREVER, &checkLock, NULL );
 
 void updateDeviceStatus(String deviceId, bool value){
-    WiFiClientSecure *wifiClientSecure = new WiFiClientSecure;
-    
-    if (wifiClientSecure) {
-        wifiClientSecure -> setCACert(rootCACertificateBaltimoreCyberTrust);
-    
-        {
-            HTTPClient https;
-            
-            Serial.print("[HTTPS] begin...\n");
-            if (https.begin(*wifiClientSecure, String(API_URL) + "/devices/" + String(deviceId) + "/log")) {
-                Serial.print("[HTTPS] POST...\n");
-                https.addHeader("Content-Type", "application/json");
-                
-                char jsonOutput[128];
-                const size_t CAPACITY = JSON_OBJECT_SIZE(1);
-                StaticJsonDocument<CAPACITY> doc;
 
-                JsonObject object = doc.to<JsonObject>();
-                object["value"] = value;
-                serializeJson(doc, jsonOutput);
+    // Create json payload
+    String messagePayload = "{\"iotDeviceId\":\"" + String(IOTHUB_DEVICEID) + "\", \"deviceId\":\"" + deviceId + "\", \"value\":" + value + "}";
+    Serial.println(messagePayload);
 
-                int httpCode = https.POST(String(jsonOutput));
-            }
-        }
-    }
+    // Send message with IoTHub
+    EVENT_INSTANCE* message = Esp32MQTTClient_Event_Generate(messagePayload.c_str(), MESSAGE);
+    Esp32MQTTClient_SendEventInstance(message);
+
 }
 
 void checkMaterial(){            
@@ -140,8 +157,16 @@ void setup() {
     Serial.begin(115200);
 
     // Connect to wifi with WiFiManager
+    Serial.println("> WiFi setup");
     WiFiManager wifiManager;
     wifiManager.autoConnect("SmartLockerSetup"); // wifi ap settings (ssid, password)
+
+    // Connect to IoTHub
+    Serial.println("> IoTHub setup");
+    Esp32MQTTClient_SetOption(OPTION_MINI_SOLUTION_NAME, "GetStarted");
+    Esp32MQTTClient_Init((const uint8_t*)IOTHUB_CONNSTRING, true);
+    Esp32MQTTClient_SetSendConfirmationCallback(SendConfirmationCallback);
+    Esp32MQTTClient_SetDeviceMethodCallback(DeviceMethodCallback);
 
     // Pin initialisation
     pinMode(RESET_WIFI_CONFIG_PIN, INPUT_PULLUP);
@@ -163,16 +188,13 @@ void loop() {
 
     // Execute tasks
     taskRunner.execute();
+    Esp32MQTTClient_Check();
 
     // Start AP with configuration portal if RESET_WIFI_CONFIG_PIN is active
     if ( !digitalRead(RESET_WIFI_CONFIG_PIN) ) {
       WiFiManager wifiManager;
       wifiManager.setTimeout(120); // sets timeout until configuration portal gets turned off in sec
       wifiManager.startConfigPortal("SmartLockerSetup"); // wifi ap settings (ssid, password)
-    }
-
-    if ( !digitalRead(BUTTON_LOCK_PIN) ) {
-        lock.openLock();
     }
 
 }
