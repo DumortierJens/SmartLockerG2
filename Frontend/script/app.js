ea2ca5c6b2395f093ec9dbf0f9fbfb6c35616ceb
Mattice Struyve <mattice.struyve@student.howest.be>
2022-01-13 11:28:49 +0100

    Finished First JS Function

--- Frontend/script/app.js
+++ Frontend/script/app.js
@@ -1,14 +1,24 @@
 'use strict';
 
-const showBestemmishowLockerngen = function(jsonObject) {
+let htmlStatus, htmlDescription, htmlSport, htmlId, htmlName;
+
+const showLocker = function (jsonObject) {
     console.log(jsonObject);
+    for (var i of jsonObject) {
+        htmlStatus = i['status']
+        htmlDescription = i['description']
+        htmlSport = i['sport']
+        htmlId = i["id"]
+        htmlName = i["name"]
+    }
+    console.log(htmlStatus, htmlDescription, htmlSport, htmlId, htmlName)
 };
 
-const init = function() {
+const start = function () {
     handleData(`https://smartlockerfunctions.azurewebsites.net/api/lockers`, showLocker);
 };
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     console.info('DOM geladen');
-    init();
+    start();
 });
