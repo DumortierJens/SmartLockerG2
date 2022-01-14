79598a0c3d3665a5264fef406061e2e8247e4977
Jens Dumortier <jens.dumortier@student.howest.be>
2022-01-14 14:31:54 +0100

    Login with facebook + get basic user details

--- Frontend/script/login.js
+++ Frontend/script/login.js
@@ -1,15 +1,55 @@
-let fbbtn;
+window.fbAsyncInit = function () {
+    FB.init({
+        appId: '614695093159122',
+        cookie: true,
+        xfbml: true,
+        version: 'v12.0'
+    });
 
-function ListenToLogin(){
-    fbbtn.addEventListener('click',function(){
-        window.location.replace(`http://${window.location.hostname}:5500/overzichtpagina.html`);
-    })
-}
+    FB.AppEvents.logPageView();
 
-function init(){
-    console.log('Dom geladen')
-    fbbtn = document.querySelector('.js-loginbtn')
-    ListenToLogin();
-}
+    FB.getLoginStatus(function (response) {
+        statusChangeCallback(response);
+    });
+};
 
-document.addEventListener('DOMContentLoaded',init)
+const statusChangeCallback = function (response) {
+    if (response.status === 'connected') {
+        console.log('Logged in with Facebook and authenticated');
+        if (document.querySelector('.js-login-page')) window.location.replace(`https://${window.location.hostname}/overzicht.html`);
+        getUserDetails();
+
+    } else {
+        console.log('Not authenticated with Facebook');
+        if (document.querySelector('.js-login-page') === null) window.location.replace(`https://${window.location.hostname}/`);
+    }
+};
+
+const checkLoginState = function () {
+    FB.getLoginStatus(function (response) {
+        statusChangeCallback(response);
+    });
+};
+
+const logout = function () {
+    FB.logout(function (response) {
+        console.log('Logged out');
+        statusChangeCallback(response);
+    });
+};
+
+const getUserDetails = function () {
+    FB.api('/me?fields=name,email,birthday,location,picture', function (response) {
+        if (response && !response.error) {
+            console.log(response);
+        }
+    });
+};
+
+(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) { return; }
+    js = d.createElement(s); js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
