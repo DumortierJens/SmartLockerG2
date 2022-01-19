58a2727ef0686f87a07b99f4c0c978dde7cbbf02
DeseckMichiel <michiel.deseck@student.howest.be>
2022-01-19 15:54:59 +0100

    change js and html and css add reservation

--- Frontend/script/addreservatie.js
+++ Frontend/script/addreservatie.js
@@ -3,25 +3,36 @@ let htmlStart;
 let htmlEnd;
 let htmlConfirm;
 
-function ListenToConfirmRegistration(){
-    htmlConfirm.addEventListener('click',function(){
+const showLockerReservation = function(jsonObject) {
+    htmlSport.innerHTML = jsonObject.sport;
+}
+
+function ListenToConfirmRegistration() {
+    htmlConfirm.addEventListener('click', function() {
         let sport = htmlSport.value;
         let start = htmlStart.value;
         let end = htmlEnd.value;
         const body = {
-            sport,start,end
+            sport,
+            start,
+            end
         }
         console.log(body)
+        handleData($ `${APIURI}/lockers/reservations`, null, null, 'POST', userToken);
     })
 }
 
-function init(){
-    console.log('Dom geladen')
-    htmlSport = document.querySelector('.js-addreg_sport')
+const getLockerReservation = function() {
+    handleData(`${APIURI}/lockers/11cf21d4-03ef-4e0a-8a17-27c26ae80abd`, showLockerReservation, null, 'GET', null, userToken);
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+    htmlSport = document.querySelector('.js-sport')
     htmlStart = document.querySelector('.js-addreg_starttime')
     htmlEnd = document.querySelector('.js-addreg_endtime')
     htmlConfirm = document.querySelector('.js-addreg_confirm')
-    ListenToConfirmRegistration()
-}
-
-document.addEventListener('DOMContentLoaded',init)
+        //const urlParams = new URLSearchParams(window.location.search);
+        //const id = urlParams.get('id');
+    getLockerReservation() //later nog met id meesturen
+        //ListenToConfirmRegistration()
+});
