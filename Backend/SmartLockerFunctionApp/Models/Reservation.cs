b7b90937c6ad4352d2abbd55d49809df22635a53
Jens Dumortier <jens.dumortier@student.howest.be>
2022-01-20 16:00:08 +0100

    reservation functions

--- Backend/SmartLockerFunctionApp/Models/Reservation.cs
+++ Backend/SmartLockerFunctionApp/Models/Reservation.cs
@@ -9,14 +9,19 @@ namespace SmartLockerFunctionApp.Models
     {
         [JsonProperty("id")]
         public Guid Id { get; set; }
+
         [JsonProperty("lockerId")]
         public Guid LockerId { get; set; }
+
         [JsonProperty("userId")]
         public string UserId { get; set; }
+
         [JsonProperty("startTime")]
         public DateTime StartTime { get; set; }
+
         [JsonProperty("endTime")]
         public DateTime EndTime { get; set; }
+
         [JsonProperty("isUsed")]
         public Boolean IsUsed { get; set; }
     }
