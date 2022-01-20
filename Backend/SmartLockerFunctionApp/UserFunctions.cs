ed5ee183f2b832a01a709df8f5b5ed327486d6d8
Jens Dumortier <jens.dumortier@student.howest.be>
2022-01-20 13:11:30 +0100

    Admin user functions

--- Backend/SmartLockerFunctionApp/UserFunctions.cs
+++ Backend/SmartLockerFunctionApp/UserFunctions.cs
@@ -14,25 +14,97 @@ namespace SmartLockerFunctionApp
 {
     class UserFunctions : AuthorizedServiceBase
     {
-        [FunctionName("GetUserDetails")]
-        public async Task<IActionResult> GetUserDetails(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "users/me")] HttpRequest req,
+        [FunctionName("GetUsers")]
+        public async Task<IActionResult> GetUsers(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "users")] HttpRequest req,
+            string userId,
             ILogger log)
         {
             try
             {
+                if (Auth.Role != "Admin")
+                    return new UnauthorizedResult();
+
+                CosmosClient cosmosClient = new CosmosClient(Environment.GetEnvironmentVariable("CosmosAdmin"));
+                Container container = cosmosClient.GetContainer("SmartLocker", "Users");
+
+                List<Models.User> users = new List<Models.User>();
+                QueryDefinition query = new QueryDefinition("SELECT * FROM Users u WHERE u.role = 'User'");
+                FeedIterator<Models.User> iterator = container.GetItemQueryIterator<Models.User>(query);
+                while (iterator.HasMoreResults)
+                {
+                    FeedResponse<Models.User> response = await iterator.ReadNextAsync();
+                    users.AddRange(response);
+                }
+
+                return new OkObjectResult(users);
+            }
+            catch (Exception ex)
+            {
+                return new StatusCodeResult(500);
+            }
+        }
+
+        [FunctionName("GetUser")]
+        public async Task<IActionResult> GetUser(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "users/{userId}")] HttpRequest req,
+            string userId,
+            ILogger log)
+        {
+            try
+            {
+                if (Auth.Role != "Admin" && userId != "me")
+                    return new UnauthorizedResult();
+                else if (userId == "me")
+                    userId = Auth.Id;
+
                 CosmosClient cosmosClient = new CosmosClient(Environment.GetEnvironmentVariable("CosmosAdmin"));
                 Container container = cosmosClient.GetContainer("SmartLocker", "Users");
 
                 Models.User user;
                 try
                 {
-                    user = await container.ReadItemAsync<Models.User>(Auth.Id, new PartitionKey(Auth.Id));
+                    user = await container.ReadItemAsync<Models.User>(userId, new PartitionKey(userId));
                 }
                 catch (CosmosException ex) when (ex.StatusCode == System.Net.HttpStatusCode.NotFound)
                 {
                     return new NotFoundResult();
                 }
+
+                return new OkObjectResult(user);
+            }
+            catch (Exception ex)
+            {
+                return new StatusCodeResult(500);
+            }
+        }
+
+        [FunctionName("BlockUser")]
+        public async Task<IActionResult> BlockUser(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "users/{userId}/block")] HttpRequest req,
+            string userId,
+            ILogger log)
+        {
+            try
+            {
+                if (Auth.Role != "Admin")
+                    return new UnauthorizedResult();
+
+                CosmosClient cosmosClient = new CosmosClient(Environment.GetEnvironmentVariable("CosmosAdmin"));
+                Container container = cosmosClient.GetContainer("SmartLocker", "Users");
+
+                Models.User user;
+                try
+                {
+                    user = await container.ReadItemAsync<Models.User>(userId, new PartitionKey(userId));
+                }
+                catch (CosmosException ex) when (ex.StatusCode == System.Net.HttpStatusCode.NotFound)
+                {
+                    return new NotFoundResult();
+                }
+
+                user.IsBlocked = true;
+                await container.ReplaceItemAsync(user, user.Id, new PartitionKey(user.Id));
 
                 return new OkObjectResult(user);
             }
