ec76f4b3347a66077f4d4a65fe0162f6d82c8949
DeseckMichiel <michiel.deseck@student.howest.be>
2022-01-21 11:42:14 +0100

    locker registration start

--- Frontend/script/addreservatie.js
+++ Frontend/script/addreservatie.js
@@ -15,18 +15,23 @@ function ListenToConfirmRegistration() {
         var start = new Date(startTime)
         var end = new Date(endTime)
         var now = new Date()
+        let error = false
         if (start > end) {
+            error = true
             console.log("Het eindtijdstip moet later liggen dan het starttijdstip.")
         }
         if (start < now) {
+            error = true
             console.log("Het starttijsstip ligt in het verleden.")
         }
         if (end < now) {
+            error = true
             console.log("Het eindtijsstip ligt in het verleden.")
         }
         var hours = Math.abs(end - start) / 36e5;
         console.log(hours)
         if (hours > 1.5) {
+            error = true
             console.log("Je hebt te lang geboekt")
         }
         const body = {
@@ -34,7 +39,11 @@ function ListenToConfirmRegistration() {
             endTime
         }
         console.log(body)
-            //handleData(`${APIURI}/reservations/11cf21d4-03ef-4e0a-8a17-27c26ae80abd`, null, null, 'POST', JSON.stringify(body), userToken);
+            //ook nog checken of er geen andere reservatie is
+        if (error = false) {
+            handleData(`${APIURI}/reservations/11cf21d4-03ef-4e0a-8a17-27c26ae80abd`, null, null, 'POST', JSON.stringify(body), userToken);
+        }
+
     })
 }
 
