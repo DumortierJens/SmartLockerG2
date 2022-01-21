ec3304434a817e167d79fde03e1d959da895e4fe
Jens Dumortier <jens.dumortier@student.howest.be>
2022-01-21 16:04:29 +0100

    Revert "Merge branch 'feature/registrationLogicBackend' into develop"
    This reverts commit c189175359f4116fa8f0f7edbab9e5e7823fd5ac.

--- Backend/SmartLockerFunctionApp/RegistrationFunctions.cs
+++ Backend/SmartLockerFunctionApp/RegistrationFunctions.cs
@@ -11,7 +11,6 @@ using SmartLockerFunctionApp.Services.Authentication;
 using SmartLockerFunctionApp.Models;
 using Microsoft.Azure.Cosmos;
 using System.Collections.Generic;
-using SmartLockerFunctionApp.Services.LockerManagement;
 
 namespace SmartLockerFunctionApp
 {
@@ -19,7 +18,7 @@ namespace SmartLockerFunctionApp
     {
         [FunctionName("StartRegistration")]
         public async Task<IActionResult> StartRegistration(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "registrations/start")] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "registration/start")] HttpRequest req,
             ILogger log)
         {
             try
@@ -27,36 +26,30 @@ namespace SmartLockerFunctionApp
                 // Get registration info
                 string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
                 Registration registration = JsonConvert.DeserializeObject<Registration>(requestBody);
-
+                
                 // Set registration defaults
                 registration.Id = Guid.NewGuid();
                 registration.UserId = Auth.Id;
                 registration.StartTime = DateTime.Now;
-                registration.EndTime = DateTime.MinValue;
-
-                // Validate start registration
-                if (!await LockerManagementService.ValidateStartRegistrationAsync(registration))
-                    return new ConflictResult();
 
                 // Get cosmos container
                 CosmosClient cosmosClient = new CosmosClient(Environment.GetEnvironmentVariable("CosmosAdmin"));
                 Container container = cosmosClient.GetContainer("SmartLocker", "Registrations");
 
                 // Save registration
-                await container.CreateItemAsync(registration, new PartitionKey(registration.Id.ToString()));
+                await container.CreateItemAsync(registration, new PartitionKey(registration.LockerId.ToString()));
 
                 return new OkObjectResult(registration);
             }
-            catch (Exception ex)
+            catch (Exception)
             {
-                throw ex;
+                throw;
             }
         }
 
         [FunctionName("StopRegistration")]
         public async Task<IActionResult> StopRegistration(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "registrations/{registrationId}/stop")] HttpRequest req,
-            Guid registrationId,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "registration/stop")] HttpRequest req,
             ILogger log)
         {
             try
@@ -73,7 +66,7 @@ namespace SmartLockerFunctionApp
                 Registration registration;
                 try
                 {
-                    registration = await container.ReadItemAsync<Registration>(registrationId.ToString(), new PartitionKey(registrationId.ToString()));
+                    registration = await container.ReadItemAsync<Registration>(registrationEnd.Id.ToString(), new PartitionKey(registrationEnd.LockerId.ToString()));
                 }
                 catch (CosmosException ex) when (ex.StatusCode == System.Net.HttpStatusCode.NotFound)
                 {
@@ -82,14 +75,10 @@ namespace SmartLockerFunctionApp
 
                 // Set registration defaults
                 registration.EndTime = DateTime.Now;
-                registration.Note = registrationEnd.Note;
-
-                // Validate end registration
-                if (!await LockerManagementService.ValidateEndRegistrationAsync(registration))
-                    return new ConflictResult();
+                registration.Note += "\nAfter:\n" + registrationEnd.Note;
 
                 // Update registration
-                await container.ReplaceItemAsync<Registration>(registration, registration.Id.ToString(), new PartitionKey(registration.Id.ToString()));
+                await container.ReplaceItemAsync<Registration>(registration, registration.Id.ToString(), new PartitionKey(registration.LockerId.ToString()));
 
                 return new OkObjectResult(registration);
             }
@@ -100,9 +89,9 @@ namespace SmartLockerFunctionApp
             }
         }
 
-        [FunctionName("GetAllRegistrations")]
-        public async Task<IActionResult> GetRegistrations(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "registrations/all")] HttpRequest req,
+        [FunctionName("GetRegistrations")]
+        public async Task<IActionResult> GetUsers(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "registrations")] HttpRequest req,
             ILogger log)
         {
             try
@@ -110,56 +99,17 @@ namespace SmartLockerFunctionApp
                 if (Auth.Role != "Admin")
                     return new UnauthorizedResult();
 
-                var registrations = await RegistrationConnector.GetRegistrationsAsync();
-
-                return new OkObjectResult(registrations);
-            }
-            catch (Exception ex)
-            {
-                return new StatusCodeResult(500);
-            }
-        }
-
-        [FunctionName("GetRegistrationsByLockerId")]
-        public async Task<IActionResult> GetRegistrationsByLockerId(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "registrations/lockers/{lockerId}")] HttpRequest req,
-            Guid lockerId,
-            ILogger log)
-        {
-            try
-            {
-                if (Auth.Role != "Admin")
-                    return new UnauthorizedResult();
-
-                var registrations = await RegistrationConnector.GetRegistrationsAsync(lockerId);
-
-                return new OkObjectResult(registrations);
-            }
-            catch (Exception ex)
-            {
-                return new StatusCodeResult(500);
-            }
-        }
-
-        [FunctionName("GetCurrentRegistrationsByUserId")]
-        public async Task<IActionResult> GetCurrentRegistrationsByUserId(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "registrations/users/{userId}")] HttpRequest req,
-            string userId,
-            ILogger log)
-        {
-            try
-            {
-                if (Auth.Role != "Admin" && userId != "me")
-                    return new UnauthorizedResult();
-                else if (userId == "me")
-                    userId = Auth.Id;
+                CosmosClient cosmosClient = new CosmosClient(Environment.GetEnvironmentVariable("CosmosAdmin"));
+                Container container = cosmosClient.GetContainer("SmartLocker", "Registrations");
 
                 List<Registration> registrations = new List<Registration>();
-                IDictionary<string, string> queryParams = req.GetQueryParameterDictionary();
-                if (Guid.TryParse(queryParams["lockerId"], out Guid lockerId))
-                    registrations.Add(await RegistrationConnector.GetCurrentRegistrationAsync(lockerId, userId));
-                else
-                    registrations.AddRange(await RegistrationConnector.GetCurrentRegistrationsAsync(userId));
+                QueryDefinition query = new QueryDefinition("SELECT * FROM Registrations r");
+                FeedIterator<Registration> iterator = container.GetItemQueryIterator<Registration>(query);
+                while (iterator.HasMoreResults)
+                {
+                    FeedResponse<Registration> response = await iterator.ReadNextAsync();
+                    registrations.AddRange(response);
+                }
 
                 return new OkObjectResult(registrations);
             }
