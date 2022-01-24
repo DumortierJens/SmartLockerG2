a230ae890098ebc16a3ba47a294fe1d8b3abf1c2
Jens Dumortier <jens.dumortier@student.howest.be>
2022-01-24 16:42:18 +0100

    remove web extention

--- Frontend/script/constants.js
+++ Frontend/script/constants.js
@@ -2,4 +2,4 @@ let APIURI = 'https://smartlockerfunctions.azurewebsites.net/api';
 // APIURI = 'http://localhost:7071/api'; // local api
 
 let WEBEXTENTION = '';
-WEBEXTENTION = '.html'; // local website
+// WEBEXTENTION = '.html'; // local website
