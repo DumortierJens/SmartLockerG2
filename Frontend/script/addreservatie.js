fc65f44eeef988fd3ac9c9e4f9ad3a9e482ab415
JarneDemoen <jarne.demoen@student.howest.be>
2022-01-24 14:57:47 +0100

    Bezig met custom time input

--- Frontend/script/addreservatie.js
+++ Frontend/script/addreservatie.js
@@ -1,8 +1,62 @@
 let htmlSport;
-let htmlStart;
+let htmlStartHour;
+let htmlStartMinute;
+let htmlEndHour;
+let htmlEndMinute;
 let htmlEnd;
 let htmlConfirm;
 let htmlDate;
+let arrayStartTimes = [];
+let arrayEndTimes = [];
+let arrayReservedHours = [];
+let arrayReservedMinutes = [];
+
+function ListenToSelectHours(){
+    htmlStartHour.addEventListener('click',function(){
+    })
+}
+
+function ModifySelect(jsonObject) {
+    console.log(jsonObject)
+    for (let reservation in jsonObject) {
+        let date = new Date(jsonObject[reservation].startTime)
+        let dateHour = date.getHours();
+        date.setHours(dateHour + (date.getTimezoneOffset() / 60))
+        let dateString = date.toLocaleTimeString()
+        arrayStartTimes.push(dateString)
+
+        date = new Date(jsonObject[reservation].endTime)
+        dateHour = date.getHours();
+        date.setHours(dateHour + (date.getTimezoneOffset() / 60))
+        dateString = date.toLocaleTimeString()
+        arrayEndTimes.push(dateString)
+    }
+    console.log("starthours", arrayStartTimes)
+    console.log("endhours", arrayEndTimes)
+    
+    ListenToSelectHours()
+}
+
+function FillOptionsSelect() {
+    for (let hour = 5; hour < 23; hour++) {
+        if (hour < 10) {
+            htmlStartHour.innerHTML += `<option value="${hour}">0${hour}</option>`
+            htmlEndHour.innerHTML += `<option value="${hour}">0${hour}</option>`
+        } else {
+            htmlStartHour.innerHTML += `<option value="${hour}">${hour}</option>`
+            htmlEndHour.innerHTML += `<option value="${hour}">${hour}</option>`
+        }
+    }
+    for (let minute = 0; minute < 60; minute++) {
+        if (minute < 10) {
+            htmlStartMinute.innerHTML += `<option value="${minute}">0${minute}</option>`
+            htmlEndMinute.innerHTML += `<option value="${minute}">0${minute}</option>`
+        } else {
+            htmlStartMinute.innerHTML += `<option value="${minute}">${minute}</option>`
+            htmlEndMinute.innerHTML += `<option value="${minute}">${minute}</option>`
+        }
+    }
+}
 
 function ShowError(htmlelement, foutboodschap) {
     console.log('Fout weergeven')
@@ -16,6 +70,8 @@ function ShowError(htmlelement, foutboodschap) {
 
 const showLockerReservation = function (jsonObject) {
     htmlSport.innerHTML = jsonObject.sport;
+    FillOptionsSelect();
+    getReservations();
     // let now = new Date(0)
     // let offset = new Date().getTimezoneOffset()
     // now.setFullYear(new Date().getFullYear())
@@ -34,17 +90,20 @@ const showLockerReservation = function (jsonObject) {
     ListenToConfirmRegistration()
 }
 
+const getReservations = function () {
+    handleData(`${APIURI}/reservations`, ModifySelect, null, 'GET', null, userToken);
+};
+
 function ListenToConfirmRegistration() {
     htmlConfirm.addEventListener('click', function () {
         var elements = document.getElementsByClassName('error_message');
         console.log(elements)
         for (let element of elements) {
             element.classList.remove("error_message")
-            if(element.nextElementSibling){
+            if (element.nextElementSibling) {
                 element.nextElementSibling.classList.remove("error_message")
-            }
-            else{
-            element.previousElementSibling.classList.remove("error_message")
+            } else {
+                element.previousElementSibling.classList.remove("error_message")
             }
         }
         let startTime = htmlStart.value;
@@ -85,8 +144,7 @@ function ListenToConfirmRegistration() {
         }
         console.log(body)
         // ook nog checken of er geen andere reservatie is
-        if (error == false) {
-            // handleData(`${APIURI}/reservations/11cf21d4-03ef-4e0a-8a17-27c26ae80abd`, null, null, 'POST', JSON.stringify(body), userToken);
+        if (error == false) { // handleData(`${APIURI}/reservations/11cf21d4-03ef-4e0a-8a17-27c26ae80abd`, null, null, 'POST', JSON.stringify(body), userToken);
         }
 
     })
@@ -98,8 +156,10 @@ const getLockerReservation = function () {
 
 document.addEventListener('DOMContentLoaded', function () {
     htmlSport = document.querySelector('.js-sport')
-    htmlStart = document.querySelector('.js-addreg_start')
-    htmlEnd = document.querySelector('.js-addreg_end')
+    htmlStartHour = document.querySelector('.js-start-hour')
+    htmlStartMinute = document.querySelector('.js-start-minute')
+    htmlEndHour = document.querySelector('.js-end-hour')
+    htmlEndMinute = document.querySelector('.js-end-minute')
     htmlConfirm = document.querySelector('.js-addreg_confirm')
     htmlDate = document.querySelector('.js-addreg_date')
     // const urlParams = new URLSearchParams(window.location.search);
