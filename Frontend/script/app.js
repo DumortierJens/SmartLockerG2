ad1227b8b8aa8dccf46f52651965e60d2cfb230f
Jens Dumortier <jens.dumortier@student.howest.be>
2022-01-25 21:44:42 +0100

    Optimize show lockers

--- Frontend/script/app.js
+++ Frontend/script/app.js
@@ -16,78 +16,31 @@ ws.onmessage = (event) => {
     }
 };
 
-const showOverview = function (jsonObject) {
-    console.log(jsonObject);
-    let htmlstring = ``;
-    let Sport,
-        sport_1,
-        sport_2,
-        klasse,
-        translate_location,
-        translate_elips,
-        translate_icon = '';
-    htmlstring += `<rect id="Bg" class="cls-1" width="360" height="592" transform="translate(0 56)"/>`;
-    for (const sport of jsonObject) {
-        console.log(sport);
+const showLockers = function (lockers) {
+    console.log(lockers);
 
-        if (sport.sport == 'Voetbal') {
-            Sport = 'Soccer';
-            sport_1 = 'soccer';
-            sport_2 = 'football';
-            klasse = 'cls-3';
-            translate_location = '-481 2';
-            translate_elips = '723 397';
-            translate_icon = '729 403';
-        }
-        if (sport.sport == 'Basketbal') {
-            Sport = 'Basketball';
-            sport_1 = 'basketball';
-            sport_2 = 'basketball';
-            klasse = 'cls-5';
-            translate_location = '-419 -226';
-            translate_elips = '699 382';
-            translate_icon = '705 388';
-        }
-        htmlstring += `<g id="${Sport}" data="${sport.id}" class="js-${sport_1}" transform="translate(${translate_location})">
-        <g id="Ellipse_1" data-name="Ellipse 1" class="js-color-${sport_1}" transform="translate(${translate_elips})">
-          <circle class="cls-6" cx="18" cy="18" r="18"/>
-          <circle class="cls-7" cx="18" cy="18" r="17.5"/>
-        </g>
-        <g id="${sport_2}" data-name="${sport_2}" class="${klasse}" transform="translate(${translate_icon})">
-          <circle class="cls-6" cx="12" cy="12" r="12"/>
-          <circle class="cls-7" cx="12" cy="12" r="11.5"/>
-        </g>
-      </g>`;
+    let htmlString = ``;
+    for (const locker of lockers) {
+        let statusClass = '';
+        if (locker.status === 'Beschikbaar')
+            statusClass = 'svg-avaible';
+        else if (locker.status === 'Bezet')
+            statusClass = 'svg-unavaible';
+        else if (locker.status === 'Buiten gebruik')
+            statusClass = 'svg-outofuse';
+
+        htmlString += `<g class="js-locker" data-id="${locker.id}" transform="translate(${locker.iconLocation})">
+            <g class="${statusClass}">
+                <circle cx="18" cy="18" r="18" />
+            </g>
+            <g style="fill: url(#${locker.sport.toLowerCase()})" transform="translate(6 6)">
+                <circle cx="12" cy="12" r="12" />
+            </g>
+        </g>`;
     }
-    htmlOverview.innerHTML = htmlstring;
-    const htmlColorFootball = document.querySelector('.js-color-soccer');
-    const htmlColorBasketball = document.querySelector('.js-color-basketball');
-    for (const sport of jsonObject) {
-        console.log(sport.status);
-        if (sport.status == 'Beschikbaar') {
-            if (sport.sport == 'Voetbal') {
-                htmlColorFootball.classList.add('svg-avaible');
-            } else {
-                htmlColorBasketball.classList.add('svg-avaible');
-            }
-        } else if (sport.status == 'Bezet') {
-            if (sport.sport == 'Voetbal') {
-                htmlColorFootball.classList.add('svg-unavaible');
-            } else {
-                htmlColorBasketball.classList.add('svg-unavaible');
-            }
-        } else if (sport.status == 'Buiten gebruik') {
-            if (sport.sport == 'Voetbal') {
-                htmlColorFootball.classList.add('svg-outofuse');
-            } else {
-                htmlColorBasketball.classList.add('svg-outofuse');
-            }
-        }
-    }
-    htmlSoccer = document.querySelector('.js-soccer');
-    htmlBasketball = document.querySelector('.js-basketball');
-    ListenToCLickSport();
-    ListenToClickProfiel();
+
+    document.querySelector('.js-overview').innerHTML += htmlString;
+    listenToLocker();
 };
 
 const showLocker = function (jsonObject) {
@@ -133,15 +86,14 @@ const showLocker = function (jsonObject) {
     }
 };
 
-const ListenToCLickSport = function () {
-    htmlSoccer.addEventListener('click', function () {
-        currentLockerID = htmlSoccer.getAttribute('data');
-        window.location.href = `${location.origin}/locker${WEBEXTENTION}?id=${currentLockerID}`;
-    });
-    htmlBasketball.addEventListener('click', function () {
-        currentLockerID = htmlBasketball.getAttribute('data');
-        window.location.href = `${location.origin}/locker${WEBEXTENTION}?id=${currentLockerID}`;
-    });
+const listenToLocker = function () {
+    const lockers = document.querySelectorAll('.js-locker');
+
+    for (const locker of lockers) {
+        locker.addEventListener('click', function () {
+            window.location.href = `${location.origin}/locker${WEBEXTENTION}?id=${this.dataset.id}`;
+        });
+    }
 };
 
 function ListenToClickToggleLocker(id) {
@@ -227,8 +179,8 @@ function listenToProfileBtn() {
     });
 }
 
-const getOverzicht = function () {
-    handleData(`${APIURI}/lockers`, showOverview, null, 'GET', null, userToken);
+const getLockers = function () {
+    handleData(`${APIURI}/lockers`, showLockers, null, 'GET', null, userToken);
 };
 
 const getLockerDetail = function (id) {
@@ -264,12 +216,17 @@ document.addEventListener('DOMContentLoaded', function () {
     htmlBackButton = document.querySelector('.js-back');
     htmlUserProfileButton = document.querySelector('.js-profile');
 
+    // Pages
+    const htmlPageOverview = document.querySelector('.js-overview-page');
+    const htmlPageLocker = document.querySelector('.js-locker-page');
+    const htmlPageProfile = document.querySelector('.js-profile-page');
+
+    // Buttons
     if (htmlBackButton) listenToBackBtn();
     if (htmlUserProfileButton) listenToProfileBtn();
 
-    if (htmlOverview) {
-        //deze code wordt gestart vanaf overzicht.html
-        getOverzicht();
+    if (htmlPageOverview) {
+        getLockers();
     }
 
     if (htmlLockerTitle) {
