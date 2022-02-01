abb43e7b0d15e2e1d0e370b0dd62ac82333f0ed7
Jens Dumortier <jens.dumortier@student.howest.be>
2022-02-01 15:41:19 +0100

    ClickSend

--- Backend/SmartLockerFunctionApp/Models/Message.cs
+++ Backend/SmartLockerFunctionApp/Models/Message.cs
@@ -7,13 +7,8 @@ namespace SmartLockerFunctionApp.Models
 {
     public class Message
     {
-        [JsonProperty("tel")]
         public string Tel { get; set; }
-        [JsonProperty("bericht")]
         public string Bericht { get; set; }
-        [JsonProperty("name")]
-        public string Name { get; set; }
-        [JsonProperty("endTime")]
         public DateTime EndTime { get; set; }
 
     }
